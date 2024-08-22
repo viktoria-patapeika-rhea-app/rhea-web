1d3e287a96ab02460c9e74dc4e6d59382e0c71e8
viktoria-patapeika-rhea-app <viktoria-patapeika-rhea-app@users.noreply.github.com>
2024-08-22 13:48:23 +0000

    deploy: 4441f55b69a9810ad502d8b6be2c6e6fc8beb42c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3188670453"
+    serviceWorkerVersion: "3565501080"
   }
 });
