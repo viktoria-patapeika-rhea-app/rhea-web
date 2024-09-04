2064ead423e43de9acd52079af6f9f4f2ad7d234
Viktoria Patapeika <viktoria.patapeika@rhea-app.com>
2024-09-04 03:05:37 +0200

    new build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1140977426"
+    serviceWorkerVersion: "1951786573"
   }
 });
